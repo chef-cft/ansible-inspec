f27acf2a08c01428965e94b571d9a53b3ac0bade
cewdesign <woodce@gmail.com>
2020-07-28 16:14:03 -0400

    added comments

--- test/integration/default/site_verify.rb
+++ test/integration/default/site_verify.rb
@@ -3,11 +3,13 @@ describe package 'httpd' do
   it { should be_installed }
 end
 
+# check if httpd is enabled and running
 describe service 'httpd' do
   it { should be_running }
   it { should be_enabled }
 end
 
+# check if port 80 is listening
 describe port 80 do
   it { should be_listening }
 end
